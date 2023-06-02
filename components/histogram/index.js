45d9c378a74894463e253b5593b16b8020280ac0
liakurianova <julia.kurianova@gmail.com>
2023-06-02 18:47:34 -0400

    improve tabs

--- components/histogram/index.js
+++ components/histogram/index.js
@@ -1,12 +1,16 @@
 const maxColAmount = 30;
-function createHistogram(colAmount) {
-    const hist = document.querySelector(".histogram__cols");
-    let histogram = "";
-    for (let i = 0; i < maxColAmount; i++) {
-        let histCol = "<div class='histogram__col' ";
-        histCol += `style="height: ${Math.round(Math.random() * 100)}%;"`;
-        histogram += histCol + "></div>";
-    }
-    hist.innerHTML = histogram;
+function createHistograms(colAmount) {
+    const histArr = document.querySelectorAll(".histogram__cols");
+
+    histArr.forEach((hist) => {
+        let histogram = "";
+        for (let i = 0; i < maxColAmount; i++) {
+            let histCol = "<div class='histogram__col' ";
+            histCol += `style="height: ${Math.round(Math.random() * 100)}%;"`;
+            histogram += histCol + "></div>";
+        }
+        hist.innerHTML = histogram;
+    });
 }
-createHistogram( maxColAmount);
+
+createHistograms( maxColAmount);
